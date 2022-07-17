f82dbb7894ae09e3b469570db155dfb8462309cf
leopoldwhite <leopoldwhite@163.com>
2022-07-17 12:50:20 +0800

    Fix BotRGCN preprocess_1 & preprocess_2

--- src/BotRGCN/cresci_15/preprocess_2.py
+++ src/BotRGCN/cresci_15/preprocess_2.py
@@ -9,7 +9,7 @@ user,tweet=fast_merge(dataset="cresci-2015")
 
 user_text=list(user['description'])
 tweet_text = [text for text in tweet.text]
-
+each_user_tweets=torch.load('./processed_data/each_user_tweets.npy')
 
 feature_extract=pipeline('feature-extraction',model='roberta-base',tokenizer='roberta-base',device=3,padding=True, truncation=True,max_length=50, add_special_tokens = True)
 
--- src/BotRGCN/twibot_20/preprocess_1.py
+++ src/BotRGCN/twibot_20/preprocess_1.py
@@ -181,3 +181,48 @@ for each in verified:
 cat_properties_tensor=default_profile_image_tensor.reshape([5301,1])
 torch.save(cat_properties_tensor,path+'cat_properties_tensor.pt')
 
+#get each_user_tweets
+user, tweet = fast_merge("Twibot-20", '209')
+
+user_index_to_uid = list(user.id)
+tweet_index_to_tid = list(tweet.id)
+        
+uid_to_user_index = {x : i for i, x in enumerate(user_index_to_uid)}
+tid_to_tweet_index = {x : i for i, x in enumerate(tweet_index_to_tid)}
+
+edge=pd.read_csv("../datasets/Twibot-20/edge.csv")
+
+edge=edge[edge.relation=='post']
+
+src = list(edge[edge["relation"] == 'post']["source_id"])
+dst = list(edge[edge["relation"] == 'post']["target_id"])
+
+new_src = []
+new_dst = []
+            
+for s, t in tqdm(zip(src, dst)):
+    new_src.append(s)
+    new_dst.append(t)
+                
+src = new_src
+dst = new_dst
+            
+src = list(map(lambda x: uid_to_user_index[x], src))
+dst = list(map(lambda x: tid_to_tweet_index[x], dst))
+
+edge['target_id']=list(map(lambda x:tid_to_tweet_index[x],edge['target_id'].values))
+
+edge['source_id']=list(map(lambda x:uid_to_user_index[x],edge['source_id'].values))
+
+
+
+dict={i:[] for i in range(len(user))}
+for i in tqdm(range(len(user))):
+    try:
+        edge['source_id'][i]
+    except KeyError:
+        continue
+    else:
+        dict[edge['source_id'][i]].append(tweet['text'][edge['target_id'][i]+len(user)])
+
+np.save('processed_data/each_user_tweets.npy',dict)
